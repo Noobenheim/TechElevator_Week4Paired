c74f747b40a4afc865c7707e994afd4a4eebead7
Kaela Glasser <kaelaglasser@gmail.com>
2019-10-07 15:01:28 -0400

    refactoring

--- module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
+++ module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
@@ -1,24 +1,14 @@
 package com.techelevator;
 
 public class KataStringCalculator {
-	
+
 	public static int add(String numbers) {
-		if( numbers.trim().equals("") ) {
+		if (numbers.trim().equals("")) {
 			return 0;
-		} else if(numbers.split(",").length == 1) {
+		} else if (numbers.split(",").length == 1) {
 			int result = Integer.parseInt(numbers);
 			return result;
-		} else if( numbers.split(",").length == 2 ) {
-			String[] splitNumbers = numbers.split(",");
-			
-			String strNumber1 = splitNumbers[0].trim();
-			String strNumber2 = splitNumbers[1].trim();
-			
-			int number1 = Integer.parseInt(strNumber1);
-			int number2 = Integer.parseInt(strNumber2);
-			
-			return number1 + number2;
-		} else if (numbers.split(",").length > 2) {
+		} else if (numbers.split(",").length > 1) {
 			String[] splitNumbers = numbers.split(",");
 			int result = 0;
 			for (String num : splitNumbers) {
@@ -27,7 +17,7 @@ public class KataStringCalculator {
 			}
 			return result;
 		}
-		
+
 		return -1;
 	}
 
