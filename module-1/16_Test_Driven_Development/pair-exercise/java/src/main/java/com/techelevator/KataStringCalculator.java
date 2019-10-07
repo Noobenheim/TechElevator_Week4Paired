89c48bdcd0cc78092bc9ed8fa3da5bef65450df4
Kaela Glasser <kaelaglasser@gmail.com>
2019-10-07 14:16:11 -0400

    calculator_returns_0_if_string_is_empty test created

--- module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
+++ module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
@@ -1,5 +1,10 @@
 package com.techelevator;
 
 public class KataStringCalculator {
+	
+	public static int add(String numbers) {
+		
+		return -1;
+	}
 
 }
--- module-1/16_Test_Driven_Development/pair-exercise/java/src/test/java/com/techelevator/KataStringCalculatorTest.java
+++ module-1/16_Test_Driven_Development/pair-exercise/java/src/test/java/com/techelevator/KataStringCalculatorTest.java
@@ -4,5 +4,12 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataStringCalculatorTest {
+	
+	@Test
+	public void calculator_returns_0_if_string_is_empty() {
+		String inputString = "";
+		int expectedReturn = 0;
+		Assert.assertEquals(expectedReturn, KataStringCalculator.add(inputString));
+	}
 
 }
