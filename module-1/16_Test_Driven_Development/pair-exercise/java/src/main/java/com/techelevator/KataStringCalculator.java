f36369afc598c6ebbebf5a72624f9331133320cd
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-07 14:18:09 -0400

    Passed test 'calculator_returns_0_if_string_is_empty'

--- module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
+++ module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
@@ -3,6 +3,9 @@ package com.techelevator;
 public class KataStringCalculator {
 	
 	public static int add(String numbers) {
+		if( numbers.trim().equals("") ) {
+			return 0;
+		}
 		
 		return -1;
 	}
