1cf3b7e7336ece71063f83512ffe0bc0017f1dcc
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-07 15:16:51 -0400

    Completed test 'calculator_works_when_numbers_are_split_by_newLine_and_commas'

--- module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
+++ module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
@@ -1,17 +1,28 @@
 package com.techelevator;
 
-public class KataStringCalculator {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class KataStringCalculator {
 	public static int add(String numbers) {
+		int sum = 0;
+		
+		List<String> splitNumbersList = new ArrayList<>();
+		// split by comma first
+		String[] commaSplits = numbers.split(",");
+		// now go through split array to split by newline
+		for( String commaSplit : commaSplits ) {
+			String[] newlineSplits = commaSplit.split("\n");
+			// add to arraylist
+			splitNumbersList.addAll(Arrays.asList(newlineSplits));
+		}
+		
 		if (numbers.trim().equals("")) {
 			return 0;
-		} else if (numbers.split(",").length == 1) {
-			int result = Integer.parseInt(numbers);
-			return result;
-		} else if (numbers.split(",").length > 1) {
-			String[] splitNumbers = numbers.split(",");
+		} else if (splitNumbersList.size() > 0) {
 			int result = 0;
-			for (String num : splitNumbers) {
+			for (String num : splitNumbersList) {
 				int strNumber = Integer.parseInt(num.trim());
 				result += strNumber;
 			}
