d67e5eab0988bd8851bb1bab2246804aa8bce651
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-07 14:43:43 -0400

    Passed 'calculator_returns_sum_of_two_numbers_in_string' test

--- module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
+++ module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/KataStringCalculator.java
@@ -9,7 +9,16 @@ public class KataStringCalculator {
 		else if(numbers.split(",").length == 1) {
 			int result = Integer.parseInt(numbers);
 			return result;
+		} else if( numbers.split(",").length == 2 ) {
+			String[] splitNumbers = numbers.split(",");
 			
+			String strNumber1 = splitNumbers[0].trim();
+			String strNumber2 = splitNumbers[1].trim();
+			
+			int number1 = Integer.parseInt(strNumber1);
+			int number2 = Integer.parseInt(strNumber2);
+			
+			return number1 + number2;
 		}
 		
 		return -1;
