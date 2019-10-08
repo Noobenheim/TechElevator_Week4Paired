6307ccc3ed3efc3f01398a90a0d27a45c50a1dc8
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-08 14:05:15 -0400

    Refactored

--- module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/NumbersToWords.java
+++ module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/NumbersToWords.java
@@ -1,15 +1,47 @@
 package com.techelevator;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class NumbersToWords {
 	public static String numbersToWords(int number) {
 		String numWord = "";
-		if (number == 0) {
-			numWord = "zero";
-		} else if( number == 1 ) {
-			numWord = "one";
-		} else if( number == 2 ) {
-			numWord = "two";
-		} 
+		if( number < 20 ) {
+			numWord = lessThanTwentyConvert(number);
+		}
 		return numWord;
+	}
+	
+	private static String lessThanTwentyConvert(int number) {
+		Map<Integer,String> map = new HashMap<>();
+		map.put(0, "zero");
+		map.put(1, "one");
+		map.put(2, "two");
+		map.put(3, "three");
+		map.put(4, "four");
+		map.put(5, "five");
+		map.put(6, "six");
+		map.put(7, "seven");
+		map.put(8, "eight");
+		map.put(9, "nine");
+		map.put(10, "ten");
+		map.put(11, "eleven");
+		map.put(12, "twelve");
+		map.put(13, "thirteen");
+		map.put(14, "fourteen");
+		map.put(15, "fifteen");
+		map.put(16, "sixteen");
+		map.put(17, "seventeen");
+		map.put(18, "eighteen");
+		map.put(19, "ninteen");
+		
+		for( Entry<Integer, String> entry : map.entrySet() ) {
+			if( entry.getKey() == number ) {
+				return entry.getValue();
+			}
+		}
+		
+		return "";
 	}
 }
