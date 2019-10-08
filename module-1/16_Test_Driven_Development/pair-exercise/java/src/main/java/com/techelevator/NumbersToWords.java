f9a084647186bf681d165e556cb019aa7bb7dc09
Dan Podobnik <daniel.podobnik@gmail.com>
2019-10-08 13:50:24 -0400

    method_returns_zero_for_0 passed

--- module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/NumbersToWords.java
+++ module-1/16_Test_Driven_Development/pair-exercise/java/src/main/java/com/techelevator/NumbersToWords.java
@@ -2,6 +2,10 @@ package com.techelevator;
 
 public class NumbersToWords {
 	public static String numbersToWords(int number) {
-		return "";
+		String numWord = "";
+		if (number == 0) {
+			numWord = "zero";
+		}
+		return numWord;
 	}
 }
